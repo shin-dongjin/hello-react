0232c80b4a4291e487abf5fb4b5b311961173c62
dongj <shin0079@algonquinlive.com>
2021-03-07 21:07:31 +0900

    Rendering multiple React elements (2-2)

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  React.createElement("div", { style: { color: "blue"}}, React.createElement("h2", null, "Ola!")),
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
