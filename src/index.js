c712e4addd289bcf79eae8b426a063b45dc6f139
dongj <shin0079@algonquinlive.com>
2021-03-10 15:52:57 +0900

    Updating with the useEffect dependency array (5.1)

--- src/index.js
+++ src/index.js
@@ -1,33 +1,30 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
 function App() {
-  const [year, setYear] = useState(2050);
-  const [manager, setManager] = useState("Rachel");
-  const [status, setStatus] = useState("Open");
+  const [val1, setVal1] = useState("");
+  const [val2, setVal2] = useState("");
+
+  useEffect(() => {
+    console.log(`field 1: ${val1}`);
+  }, [val1]);
+
+  useEffect(() => {
+    console.log(`field 2: ${val2}`);
+  }, [val1, val2]);
+
   return (
     <>
-      <div>
-        <h1>Year: {year}</h1>
-        <button onClick={() => setYear(year+1)}>Plus</button>
-      </div>
-      <div>
-        <h1>Manager on duty: {manager}</h1>
-        <button onClick={() => setManager("Philip")}>Change</button>
-      </div>
-      <div>
-        <h1>Status: {status}</h1>
-        <button onClick={() => setStatus("Open")}>
-          Open
-        </button>
-        <button onClick={() => setStatus("Back in 5")}>
-          Break
-        </button>
-        <button onClick={() => setStatus("Closed")}>
-          Closed
-        </button>
-      </div>
+      <label>
+        First Phase:
+        <input value={val1} onChange={e => setVal1(e.target.value)}></input>
+      </label>
+      <br />
+      <label>
+        Second Phase:
+        <input value={val2} onChange={e => setVal2(e.target.value)}></input>
+      </label>
     </>
   );
 }
